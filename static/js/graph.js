e8675a05b048c8237758a2726eb722bb95546112
Mark Sheehan <marksheehan72@hotmail.com>
2019-01-03 20:17:12 +0000

    scatter plots created

--- static/js/graph.js
+++ static/js/graph.js
@@ -10,15 +10,17 @@ function makeGraphs(error, studentData) {
         d.math_score = parseInt(d.math_score);
         d.reading_score = parseInt(d["reading_score"]);
         d.writing_score = parseInt(d["writing_score"]);
-        d.average_score = (d.math_score + d.reading_score + d.writing_score) / 3;
     });
 
-    
+
     show_gender_balance(ndx);
     show_test_scores_by_gender(ndx);
     show_parental_level_of_education_selector(ndx);
     show_race_ethnicity_balance(ndx);
     show_percent_that_are_in_each_race(ndx);
+    show_math_score_to_reading_score_correlation(ndx);
+    show_math_score_to_writing_score_correlation(ndx);
+    show_reading_score_to_writing_score_correlation(ndx);
 
     dc.renderAll();
 }
@@ -26,13 +28,20 @@ function makeGraphs(error, studentData) {
 
 
 function show_gender_balance(ndx) {
-    var genderDim = ndx.dimension(dc.pluck("gender"));
+    var genderColors = d3.scale.ordinal()
+        .domain(["Female", "Male"])
+        .range(["pink", "blue"]);
+    var genderDim = ndx.dimension(function(d) {
+        return [d.gender];
+    });
     var genderMix = genderDim.group();
 
     dc.barChart("#gender-balance")
         .width(350)
         .height(250)
         .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .colorAccessor(function(d) {return d.key[0];})
+        .colors(genderColors)
         .dimension(genderDim)
         .group(genderMix)
         .transitionDuration(500)
@@ -45,32 +54,43 @@ function show_gender_balance(ndx) {
 
 
 function show_test_scores_by_gender(ndx) {
-    var genderDim = ndx.dimension(dc.pluck("gender"));
+    var genderColors = d3.scale.ordinal()
+        .domain(["Female", "Male"])
+        .range(["pink", "blue"]);
+    var genderDim = ndx.dimension(function(d) {
+        return [d.gender];
+    });
     var math_score_by_gender = genderDim.group().reduceSum(dc.pluck('math_score'));
     var reading_score_by_gender = genderDim.group().reduceSum(dc.pluck('reading_score'));
     var writing_score_by_gender = genderDim.group().reduceSum(dc.pluck('writing_score'));
 
     dc.pieChart("#gender-balance-math")
-                .height(200)
-                .radius(90)
-                .transitionDuration(500)
-                .dimension(genderDim)
-                .group(math_score_by_gender)
-    
+        .height(200)
+        .radius(90)
+        .transitionDuration(500)
+        .colorAccessor(function(d) { return d.key[0]; })
+        .colors(genderColors)
+        .dimension(genderDim)
+        .group(math_score_by_gender)
+
     dc.pieChart("#gender-balance-reading")
-                .height(200)
-                .radius(90)
-                .transitionDuration(500)
-                .dimension(genderDim)
-                .group(reading_score_by_gender)
+        .height(200)
+        .radius(90)
+        .transitionDuration(500)
+        .colorAccessor(function(d) { return d.key[0]; })
+        .colors(genderColors)
+        .dimension(genderDim)
+        .group(reading_score_by_gender)
 
     dc.pieChart("#gender-balance-writing")
-                .height(200)
-                .radius(90)
-                .transitionDuration(500)
-                .dimension(genderDim)
-                .group(writing_score_by_gender)    
-    
+        .height(200)
+        .radius(90)
+        .transitionDuration(500)
+        .colorAccessor(function(d) { return d.key[0]; })
+        .colors(genderColors)
+        .dimension(genderDim)
+        .group(writing_score_by_gender)
+
 }
 
 
@@ -85,13 +105,20 @@ function show_parental_level_of_education_selector(ndx) {
 
 
 function show_race_ethnicity_balance(ndx) {
-    var race_ethnicityDim = ndx.dimension(dc.pluck("race_ethnicity"));
+    var raceColors = d3.scale.ordinal()
+        .domain(["A", "B", "C", "D", "E"])
+        .range(["red", "orange", "yellow", "green", "blue"]);
+    var race_ethnicityDim = ndx.dimension(function(d) {
+        return [d.race_ethnicity];
+    });
     var race_ethnicityMix = race_ethnicityDim.group();
 
     dc.barChart("#race_ethnicity-graph")
         .width(350)
         .height(250)
         .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .colorAccessor(function(d, i) { return i; })
+        .colors(raceColors)
         .dimension(race_ethnicityDim)
         .group(race_ethnicityMix)
         .transitionDuration(500)
@@ -104,24 +131,24 @@ function show_race_ethnicity_balance(ndx) {
 
 
 function show_percent_that_are_in_each_race(ndx) {
-    
+
     function parental_level_of_education_by_Race(dimension, parental_level_of_education) {
         return dimension.group().reduce(
-            function (p, v) {
+            function(p, v) {
                 p.total++;
                 if (v.parental_level_of_education === parental_level_of_education) {
                     p.match++;
                 };
                 return p;
             },
-            function (p, v) {
+            function(p, v) {
                 p.total--;
                 if (v.parental_level_of_education === parental_level_of_education) {
                     p.match--;
                 };
                 return p;
             },
-            function () {
+            function() {
                 return { total: 0, match: 0 }
             }
         );
@@ -134,7 +161,7 @@ function show_percent_that_are_in_each_race(ndx) {
     var associateDegreeProfByRace = parental_level_of_education_by_Race(dim, "associate's degree");
     var bachelorDegreeProfByRace = parental_level_of_education_by_Race(dim, "bachelor's degree");
     var mastersDegreeProfByRace = parental_level_of_education_by_Race(dim, "master's degree");
-    
+
 
     dc.barChart("#parental_level_of_education_by_Race-chart")
         .width(350)
@@ -146,10 +173,11 @@ function show_percent_that_are_in_each_race(ndx) {
         .stack(associateDegreeProfByRace, "associate's degree")
         .stack(bachelorDegreeProfByRace, "bachelor's degree")
         .stack(mastersDegreeProfByRace, "master's degree")
-        .valueAccessor(function (d) {
-            if(d.value.total > 0) {
+        .valueAccessor(function(d) {
+            if (d.value.total > 0) {
                 return (d.value.match / d.value.total) * 100
-            } else {
+            }
+            else {
                 return 0;
             }
             return d.value.percent * 100;
@@ -158,5 +186,113 @@ function show_percent_that_are_in_each_race(ndx) {
         .xUnits(dc.units.ordinal)
         .xAxisLabel("Race")
         .legend(dc.legend().x(270).y(20).itemHeight(15).gap(5))
-        .margins({top: 10, right: 100, bottom: 30, left: 30});
+        .margins({ top: 10, right: 100, bottom: 30, left: 30 });
+}
+
+
+function show_math_score_to_reading_score_correlation(ndx) {
+    var genderColors = d3.scale.ordinal()
+        .domain(["Female", "Male"])
+        .range(["pink", "blue"]);
+
+    var mathDim = ndx.dimension(dc.pluck("math_score"));
+    var scoresDim = ndx.dimension(function(d) {
+        return [d.math_score, d.reading_score, d.gender];
+    });
+    var scoresGroup = scoresDim.group();
+
+    var minMath = mathDim.bottom(1)[0].math_score;
+    var maxMath = mathDim.top(1)[0].math_score;
+
+    dc.scatterPlot("#math_vs_reading_scores")
+        .width(600)
+        .height(400)
+        .x(d3.scale.linear().domain([minMath, maxMath]))
+        .brushOn(false)
+        .symbolSize(8)
+        .clipPadding(10)
+        .yAxisLabel("Reading Score")
+        .xAxisLabel("Math Score")
+        .title(function(d) {
+            return "This person received " + d.key[0] + " in Math and " + d.key[1] + " in Reading.";
+        })
+        .colorAccessor(function(d) {
+            return d.key[2];
+        })
+        .colors(genderColors)
+        .dimension(scoresDim)
+        .group(scoresGroup)
+        .margins({ top: 10, right: 50, bottom: 75, left: 75 });
+}
+
+
+function show_math_score_to_writing_score_correlation(ndx) {
+    var genderColors = d3.scale.ordinal()
+        .domain(["Female", "Male"])
+        .range(["pink", "blue"]);
+
+    var mathDim = ndx.dimension(dc.pluck("math_score"));
+    var scoresDim = ndx.dimension(function(d) {
+        return [d.math_score, d.writing_score, d.gender];
+    });
+    var scoresGroup = scoresDim.group();
+
+    var minMath = mathDim.bottom(1)[0].math_score;
+    var maxMath = mathDim.top(1)[0].math_score;
+
+    dc.scatterPlot("#math_vs_writing_scores")
+        .width(600)
+        .height(400)
+        .x(d3.scale.linear().domain([minMath, maxMath]))
+        .brushOn(false)
+        .symbolSize(8)
+        .clipPadding(10)
+        .yAxisLabel("Writing Score")
+        .xAxisLabel("Math Score")
+        .title(function(d) {
+            return "This person received " + d.key[0] + " in Math and " + d.key[1] + " in Writing.";
+        })
+        .colorAccessor(function(d) {
+            return d.key[2];
+        })
+        .colors(genderColors)
+        .dimension(scoresDim)
+        .group(scoresGroup)
+        .margins({ top: 10, right: 50, bottom: 75, left: 75 });
+}
+
+
+function show_reading_score_to_writing_score_correlation(ndx) {
+    var genderColors = d3.scale.ordinal()
+        .domain(["Female", "Male"])
+        .range(["pink", "blue"]);
+
+    var readingDim = ndx.dimension(dc.pluck("reading_score"));
+    var scoresDim = ndx.dimension(function(d) {
+        return [d.reading_score, d.writing_score, d.gender];
+    });
+    var scoresGroup = scoresDim.group();
+
+    var minReading = readingDim.bottom(1)[0].reading_score;
+    var maxReading = readingDim.top(1)[0].reading_score;
+
+    dc.scatterPlot("#reading_vs_writing_scores")
+        .width(600)
+        .height(400)
+        .x(d3.scale.linear().domain([minReading, maxReading]))
+        .brushOn(false)
+        .symbolSize(8)
+        .clipPadding(10)
+        .yAxisLabel("Writing Score")
+        .xAxisLabel("Reading Score")
+        .title(function(d) {
+            return "This person received " + d.key[0] + " in Reading and " + d.key[1] + " in Writing.";
+        })
+        .colorAccessor(function(d) {
+            return d.key[2];
+        })
+        .colors(genderColors)
+        .dimension(scoresDim)
+        .group(scoresGroup)
+        .margins({ top: 10, right: 50, bottom: 75, left: 75 });
 }
